a4c41c09b6b9daf3c75443f773eec89c128302e8
Yarles Torres <44686980+YarlesTorres@users.noreply.github.com>
2020-07-28 15:52:53 -0300

    Add files via upload

---
+++ 01-fundamentos-basicos.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Fundamento básicos PHP</title>
+</head>
+<body>
+	<?php
+		$nome = "Yarles";
+		$idade = 23;
+		echo "Meu nome é: ".$nome;
+		print("<br>minha idade é: ".$idade);
+	?>
+</body>
+</html>
