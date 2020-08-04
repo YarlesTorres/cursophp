bc1ffa9142689474400874d7d4aef894bfff1b88
Yarles Torres <44686980+YarlesTorres@users.noreply.github.com>
2020-08-04 20:28:18 -0300

    Add files via upload

---
+++ switch-case.php
@@ -0,0 +1,17 @@
+<?php
+	$cor = "Vermelho";
+
+	switch ($cor) {
+		case 'Vermelho':
+			echo "Sua cor favorita é o Vermelho";
+			break;
+		case 'Verde':
+			echo "Sua cor favorita é o Verde";
+			break;
+		case 'Azul':
+			echo "Sua cor favorita é o Azul";
+			break;
+		default:
+			echo "Não possui cor preferida!";
+			break;
+	}
