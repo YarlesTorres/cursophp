4c6a123e63436bd19d6176574f0652d289884605
Yarles Torres <44686980+YarlesTorres@users.noreply.github.com>
2020-08-11 21:35:09 -0300

    Create 01-formulario.php

---
+++ forms/01-formulario.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>01-Formulários PHP</title>
+</head>
+<body>
+
+	<!-- Basta mudar o arquivo de dados e o metodo -->
+	<form action="dados-get.php" method="get">
+		Nome: <input type="text" name="nome"><br />
+		E-mail: <input type="email" name="email"><br />
+		<input type="submit" name="enviar">
+	</form>
+</body>
+</html>
